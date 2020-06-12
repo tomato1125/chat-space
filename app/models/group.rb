5ea330d3d8fc380196f5ebfa964dca6a475112d1
tomato1125 <naokiyoshida1125@icoud.com>
2020-06-12 12:08:32 +0900

    message_send_function edit

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,19 @@
 class Group < ApplicationRecord
   has_many :user_groups
   has_many :users, through: :user_groups
-  has_many :tweets
+  has_many :messages
+
   validates :name, presence: true, uniqueness: true
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      if last_message.content?
+        last_message.content
+      else
+        '画像が投稿されています'
+      end
+    else
+      'まだメッセージはありません。'
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_11_001656) do
+ActiveRecord::Schema.define(version: 2020_06_11_152341) do
 
   create_table "groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -19,15 +19,15 @@ ActiveRecord::Schema.define(version: 2020_06_11_001656) do
     t.index ["name"], name: "index_groups_on_name", unique: true
   end
 
-  create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "text"
+  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content"
     t.string "image"
     t.bigint "group_id"
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["group_id"], name: "index_tweets_on_group_id"
-    t.index ["user_id"], name: "index_tweets_on_user_id"
+    t.index ["group_id"], name: "index_messages_on_group_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "user_groups", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -51,8 +51,8 @@ ActiveRecord::Schema.define(version: 2020_06_11_001656) do
     t.index ["name"], name: "index_users_on_name", unique: true
   end
 
-  add_foreign_key "tweets", "groups"
-  add_foreign_key "tweets", "users"
+  add_foreign_key "messages", "groups"
+  add_foreign_key "messages", "users"
   add_foreign_key "user_groups", "groups"
   add_foreign_key "user_groups", "users"
 end
