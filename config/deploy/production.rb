b72529ac390aa5cd5c0316f09a5848e6409a74f6
tomato1125 <naokiyoshida1125@icoud.com>
2020-06-18 12:34:28 +0900

    deploy(2)

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,3 +59,5 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+
+server '18.180.238.247', user: 'ec2-user', roles: %w{app db web}
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,13 +1,11 @@
-#サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
 app_path = File.expand_path('../../../', __FILE__)
 
 worker_processes 1
 
 working_directory "#{app_path}/current"
 
-listen "#{app_path}/tmp/sockets/unicorn.sock"
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 pid "#{app_path}/shared/tmp/pids/unicorn.pid"
-
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
